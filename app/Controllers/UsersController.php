3c0b86399d3fd77f75f18a7b708d8b32cda13294
dacePetra <dace.petra@gmail.com>
2022-02-26 03:23:28 +0200

    update

---
+++ app/Controllers/UsersController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controllers;
+
+use App\View;
+
+class UsersController
+{
+    public function index(array $vars) //RESTful API, show all users
+    {
+        //var_dump('show all users');
+        //get information from database
+        //create array with Article objects
+        return new View('Users/index.html', [
+            'articles' => []
+        ]);
+    }
+
+    public function show(array $vars)
+    {
+        // var_dump("show single user - {$input['id']}");
+        //get information from database where article ID = $vars['id']
+        // create Article object
+        //give template for rendering
+
+        return new View('Users/show.html', [
+            'id'=>$vars['id']
+        ]);
+    }
+}
--- index.php
+++ index.php
@@ -1,2 +1,53 @@
 <?php
+
+use App\Controllers\UsersController;
+use App\View;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
 require_once 'vendor/autoload.php';
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/users', [\App\Controllers\UsersController::class, 'index']);  // 'App\Controllers\UsersController'
+    $r->addRoute('GET', '/users/{id:\d+}', [\App\Controllers\UsersController::class, 'show']); //handler: 'App\Controllers\UsersController->show'
+}); //Šeit definē adreses un ko tālāk darīt
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        var_dump("404 Not Found");
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        var_dump("405 Method Not Allowed");
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];   //$handler = explode('->', $routeInfo[1]);
+        $controller = $handler[0];
+        $method = $handler[1];
+
+        $vars = $routeInfo[2];
+
+        /** @var View $response */ //lai var getPath un getVariables pasaukt
+        $response = (new $controller)->$method($vars);
+
+        $loader = new FilesystemLoader('app/Views'); //filename path
+        $twig = new Environment($loader); //noņēmām kašošanu
+
+        //if($response instanceof View)
+        echo $twig->render($response->getPath(), $response->getVariables());
+        break;
+}
+
